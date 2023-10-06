946021e658c964763d045360e6b593935be677bf
3aljeet <ytbarek45@icloud.com>
2023-10-05 23:25:35 -0700

    added files and folders, as well as code from the 1st react app we made

---
+++ client/src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// Here we are importing a CSS file as a dependency
+
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>EATERY-APP</h1>
+    </header>
+  );
+}
+
+export default Header;
