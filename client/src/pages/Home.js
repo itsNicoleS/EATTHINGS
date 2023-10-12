c345a76d2f93d742ce8edbe4aa6006635ee815ba
nolli <nolliepolieollie@gmail.com>
2023-10-11 19:12:00 -0700

    finishing wireframing

---
+++ client/src/pages/Home.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Restaurant Name</h1>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+
+      <section className="FeaturedMenu">
+        <h2>Featured Menu</h2>
+        {/* Featured menu items go here */}
+        <div className="MenuItem">
+          <img src="food-item-1.jpg" alt="Food Item 1" />
+          <h3>Food Item 1</h3>
+          <p>Description of Food Item 1.</p>
+        </div>
+        <div className="MenuItem">
+          <img src="food-item-2.jpg" alt="Food Item 2" />
+          <h3>Food Item 2</h3>
+          <p>Description of Food Item 2.</p>
+        </div>
+      </section>
+
+      <section className="Contact">
+        <h2>Contact Us</h2>
+        <p>For reservations and inquiries, please contact us at:</p>
+        <p>Phone: 123-456-7890</p>
+        <p>Email: info@example.com</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
