952b2e5dc4eed6702ff299202e15304811c2557a
nolli <nolliepolieollie@gmail.com>
2023-10-11 20:13:50 -0700

    made seeds, and working on routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, } from 'react-router-dom';
 import './App.css';
+import Welcome from './components/Welcome';
+import card from '.card.js';
+// import Contact from './components/Contact';
+// import SignUp from './components/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+       
+          <Route path="/welcome" component={Welcome} />
+          {/* <Route path="/menu" component={Menu} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/signup" component={SignUp} /> */}
+
+     
+      
+        
+      </div>
+    </Router>
   );
 }
 
