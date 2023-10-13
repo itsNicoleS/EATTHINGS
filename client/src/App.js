0d795fe6775844d960a4e9d68c87ffd05a2c8c7c
3aljeet <ytbarek45@icloud.com>
2023-10-12 21:33:02 -0700

    installed needed technologies, wored on serverjs, auth.js, connectionjs, also began on the typeDefs

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Welcome from './components/Welcome';
-import Card from './components/card.js';
+import Card from './components/card';
+import Navbar from './components/navbar'
 
 // import Contact from './components/Contact';
 // import SignUp from './components/SignUp';
 
-export default function Display() {
+function App() {
   return (
     <div className="App">
   
       <Router>
         <Routes>
-          <Route path="/navbar" element={<navbar />} />
-          <Route path="/welcome" element={<Welcome />} />
-          <Route path="/card" element={<Card />} />
+          <Route path="/navbar" element={< Navbar />} />
+          <Route path="/welcome" element={< Welcome />} />
+          <Route path="/card" element={< Card />} />
           {/*
           <Route path="/contact" component={Contact} />
           <Route path="/signup" component={SignUp} /> */}
@@ -27,4 +28,4 @@ export default function Display() {
   );
 }
 
-
+export default App;
---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const Category = require('./Category');
+const Meal = require('./Meal');
+const Menu = require('./Menu');
+const User = require('./User');
+
+module.exports = { Category, Meal, Menu, User };
